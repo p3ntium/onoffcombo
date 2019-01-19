18bc72f3dad8136dc0373c7d11a392e8e04e0011
p3ntium <pxntium@gmail.com>
2019-01-19 23:53:10 +0100

    Create configuracion.h
    Fichero de configuración

---
+++ configuracion.h
@@ -0,0 +1,5 @@
+const char* ssid = "your_ssid";
+const char* password = "your_wifi_password";
+IPAddress ip(0, 0, 0, 0); // La IP de este ESP8266 (LO UNICO A CAMBIAR GENERALMENTE)
+IPAddress gateway(0, 0, 0, 0); // La IP del domohub
+IPAddress subnet(255, 255, 255, 0);
